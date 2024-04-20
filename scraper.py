5b2f3645ecae153b803d6828371258a8f5bde3ff
Benjamin Irwin <benjamintdirwin@gmail.com>
2024-04-20 19:46:46 +0100

    Changes to scraper.py

--- scraper.py
+++ scraper.py
@@ -1,3 +1,5 @@
+import json
+import os
 import time
 from datetime import datetime
 
@@ -7,44 +9,77 @@ import requests
 import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
 print('Setting up SQS and SNS services...')
 
-# Set up AWS services
-sns_client = boto3.client('sns', region_name='eu-west-2')
-sqs_client = boto3.client('sqs', region_name='eu-west-2')
-sqs_queue_url = 'https://sqs.eu-west-2.amazonaws.com/637423486927/StatusQueue.fifo'
 
 def get_public_ip():
     """ Retrieve the public IP address of the current server. """
     response = requests.get('https://api.ipify.org')
     return response.text
 
-def handle_sns_message():
-    # Wait until we have received a message from SNS
+
+public_ip_address = get_public_ip()
+
+# Set up AWS services
+sns_client = boto3.client('sns', region_name='eu-west-2')
+sqs_client = boto3.client('sqs', region_name='eu-west-2')
+sqs_status_url = 'https://sqs.eu-west-2.amazonaws.com/637423486927/StatusQueue.fifo'
+
+sqs_target_url = os.environ.get('SQS_TARGET_URL')
 
 
-    # This is a placeholder. In practice, you would set this up to be triggered by an actual SNS message.
-    # Here we assume you have the message containing the URL
-    message = {'url': 'https://twitter.com/home'}
-    return message['url']
+def get_public_ip():
+    """ Retrieve the public IP address of the current server. """
+    response = requests.get('https://api.ipify.org')
+    return response.text
 
-def publish_to_sqs(url, ip_address):
+
+def listen_for_sqs_change(wait_time = 1):
+    response = sqs_client.receive_message(
+        QueueUrl=sqs_target_url,
+        AttributeNames=['All'],
+        MaxNumberOfMessages=10,
+        WaitTimeSeconds=wait_time  # Adjust this as needed
+    )
+
+    messages = response.get('Messages', [])
+    if messages:
+        # Get the latest message
+        message = messages[-1]
+        message_body = json.loads(message['Body'])
+        url = message_body.get('Message')
+        return url
+
+    return None
+
+
+
+
+def publish_to_sqs(ip_address, status):
     """ Publish a message to SQS with the status of the page load. """
     message_body = {
-        "url": url,
-        "status": "loaded",
-        "link": f"https://{ip_address}:6080/vnc.html?host={ip_address}&port=6080"
+        "status": status,
+        "ip": ip_address
     }
-    sqs_client.send_message(QueueUrl=sqs_queue_url,
+    sqs_client.send_message(QueueUrl=sqs_status_url,
                             MessageBody=str(message_body),
                             MessageGroupId=str(datetime.now().timestamp()))
 
 
 print('Setting up chromedriver...')
 
+publish_to_sqs(public_ip_address, 'startup')
+
 # Main flow
 driver = uc.Chrome()
-url = handle_sns_message()
+url = None
+
+while not url:
+    url = listen_for_sqs_change()
 
 print('Navigating to:', url)
 
@@ -52,24 +87,33 @@ driver.get(url)
 
 loaded = False
 
+publish_to_sqs(public_ip_address, 'loading')
+
 # Start a loop that will refresh the page every 2.5 seconds
 while not loaded:
-    print('Refreshing...')
-    # Check if the page has loaded by looking for any text or a specific element
-    # Example: Check if there is any text in the body
-    # if driver.find_element_by_tag_name('body').text.strip() != '':
-    if driver.find_element(By.TAG_NAME, 'body').text.strip() != '':
-        print("Page has loaded content, exiting the loop.")
-        loaded = True
-        break
-    # Refresh the page
-    driver.refresh()
-    # Wait for 2.5 seconds
-    time.sleep(2.5)
+    new_url = listen_for_sqs_change()
+    if new_url:
+        print('New URL detected:', new_url)
+        driver.get(new_url)
+        url = new_url
+    try:
+        print('Refreshing...')
+        # Check if the page has loaded by looking for any text or a specific element
+        # Example: Check if there is any text in the body
+        # if driver.find_element_by_tag_name('body').text.strip() != '':
+        if driver.find_element(By.TAG_NAME, 'body').text.strip() != '':
+            print("Page has loaded content, exiting the loop.")
+            loaded = True
+            break
+        # Refresh the page
+        driver.refresh()
+        # Wait for 2.5 seconds
+        time.sleep(2.5)
+    except Exception as e:
+        publish_to_sqs(public_ip_address, 'failure')
 
 if loaded:
-    public_ip_address = get_public_ip()
-    publish_to_sqs(url, public_ip_address)
+    publish_to_sqs(public_ip_address, 'success')
 
     # Wait indefinitely to keep the browser open
     while True:
